b6f420feb0e16e29bca440b7e697850d2f62d857
Bread-Jin <55316494+Bread-Jin@users.noreply.github.com>
2020-11-09 19:31:02 +0900

    Update fcnet.py

--- fcnet.py
+++ fcnet.py
@@ -49,11 +49,11 @@ class netC1(nn.Module):
         nn.LeakyReLU(0.2, inplace=True),
         nn.Conv1d(ndf, nc, kernel_size=1, bias=True),
         )
-        print (d,ndf,nc)
+#        print (d,ndf,nc)
 
     def forward(self, input):
         tc = self.trunk(input)
         ce = self.head(tc)
-        print (tc.shape, ce.shape)
-        print ('\n')
+#        print (tc.shape, ce.shape)
+#        print ('\n')
         return tc, ce
