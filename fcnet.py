9dae1c84b74fe007b85ab95c09dc84f57fc66e11
Bread-Jin <55316494+Bread-Jin@users.noreply.github.com>
2020-11-26 22:52:33 +0900

    Update fcnet.py

--- fcnet.py
+++ fcnet.py
@@ -47,6 +47,10 @@ class netC1(nn.Module):
         nn.Conv1d(d, ndf, kernel_size=1, bias=False),
         nn.LeakyReLU(0.2),
         nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
 #       Below line is for the BN process
 #        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
 #        nn.BatchNorm1d(ndf),
